948dae9938f63496faafb54677d52fd9f09f6bff
NataliaKonoz <Nataliia.Konoz@cs.khpi.edu.ua>
2024-09-16 14:04:52 +0300

    add tasks 6 and 7

---
+++ task_6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float x, y;
+
+
+        Console.Write("Введiть координату x: ");
+        x = float.Parse(Console.ReadLine());
+
+
+        Console.Write("Введiть координату y: ");
+        y = float.Parse(Console.ReadLine());
+
+                                                                        // Перевірка для першої чверті 
+        if (x > 0 && y > 0 && (x * x + y * y <= 1))
+        {
+            Console.WriteLine("true");
+        }
+                                                                        // Перевірка для третьої чверті
+        else if (x >= -1 && x <= 0 && y >= -1 && y <= 0)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+    }
+}
---
+++ task_7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float x, y;
+        Console.Write("Введiть координату x: ");
+        x = float.Parse(Console.ReadLine());
+
+        Console.Write("Введiть координату y: ");
+        y = float.Parse(Console.ReadLine());
+
+                                                                                 // Перевірка для першої чверті 
+        bool isInsideCircle = (x > 0 && y > 0 && (x * x + y * y <= 1));
+
+                                                                                 // Перевірка для третьої чверті
+        bool isInsideSquare = (x >= -1 && x <= 0 && y >= -1 && y <= 0);
+
+  
+        bool result = isInsideCircle || isInsideSquare;
+
+        Console.WriteLine(result ? "true" : "false");
+    }
+}
