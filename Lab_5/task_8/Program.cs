70e5bd1f67104e4a9dfa6f7349439156845f051a
NataliaKonoz <Nataliia.Konoz@cs.khpi.edu.ua>
2024-10-20 17:46:38 +0300

    Add lab 5 t8

---
+++ Lab_5/task_8/Program.cs
@@ -0,0 +1,12 @@
+namespace task_1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new Form1());
+        }
+    }
+}
